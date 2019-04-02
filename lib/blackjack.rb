fc72f09ac9d901b16f5c789cc549edee2154e198
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-02 05:59:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,7 @@ def end_game(card_total)
 end
 
 def initial_round
-  first_round =  deal_card + deal_card
+  first_round = deal_card + deal_card
   display_card_total(first_round)
   return first_round
 end
